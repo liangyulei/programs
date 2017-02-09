4a62341eb6eb872cd9e6c4ae94157378514473e8
Zhi J Teoh <cs30xjk@ieng9.ucsd.edu>
2017-02-08 20:36:37 -0800

    milestone update

--- pa2/printChar.s
+++ pa2/printChar.s
@@ -1,5 +1,20 @@
+/*
+Filename : printChar.s
+Author : Zhi Jia Teoh
+Date : Feb 8 2017
+UserID : cs30xjk
+Description : prints char
+*/
+
 .global printChar
 .section ".data"
+
+/*
+Function name : printChar(char ) 
+Description : Prints according to the format from arg value
+Return : void
+*/
+
 fmt:
 .asciz "%c"
 
